8de9a5fb378ce93a7e55c707486604a4ce382c36
LiuKaiLong <672598974@qq.com>
2020-01-05 03:32:22 +0800

    :sparkles: 更改 Reducer 逻辑

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -1,3 +1,33 @@
+function apendARule2Relation(relation: RulesRelation) {
+  relation.rules = [
+    ...relation.rules,
+    {
+      type: 'profile_rule',
+      field: 'province',
+      params: [],
+    },
+  ];
+}
+function cantBeRelation(relation: RulesRelation) {
+  return relation.rules.length <= 1;
+}
+function relation2Rule(relation: RulesRelation) {
+  let temp = relation.rules[0];
+  delete relation.relation;
+  delete relation.rules;
+
+  ((relation as unknown) as Rule).type = 'profile_rule';
+  ((relation as unknown) as Rule).field = (temp as Rule).field;
+  ((relation as unknown) as Rule).params = (temp as Rule).params;
+}
+function rule2Relation(rule: Rule) {
+  ((rule as unknown) as RulesRelation).rules = [{ ...rule }];
+  apendARule2Relation((rule as unknown) as RulesRelation);
+  delete (rule as Rule).field;
+  delete (rule as Rule).params;
+  ((rule as unknown) as RulesRelation).type = 'rules_relation';
+  ((rule as unknown) as RulesRelation).relation = 'and';
+}
 const reducer = (state: RulesRelation, action: action) => {
   const { type, positon } = action;
   console.log(type);
@@ -12,28 +42,21 @@ const reducer = (state: RulesRelation, action: action) => {
 
   switch (type) {
     case 'ADD_A_RULE':
-      (currentItem as RulesRelation).rules.push({
-        type: 'profile_rule',
-        field: 'province',
-        params: [],
-      });
+      apendARule2Relation(currentItem as RulesRelation);
       return { ...state };
     case 'ADD_A_RULES_RELATION':
-      (currentItem as RulesRelation).rules = [
-        { ...currentItem },
-        {
-          type: 'profile_rule',
-          field: 'province',
-          params: [],
-        },
-      ];
-      delete (currentItem as Rule).field;
-      delete (currentItem as Rule).params;
-      (currentItem as RulesRelation).type = 'rules_relation';
-      (currentItem as RulesRelation).relation = 'and';
+      rule2Relation(currentItem as Rule);
       return { ...state };
     case 'REMOVE_A_RULE':
       (upperItem as RulesRelation).rules.splice(positon[positon.length - 1], 1);
+      if (cantBeRelation(upperItem as RulesRelation)) {
+        if ((upperItem as RulesRelation).rules[0].type === 'profile_rule') {
+          relation2Rule(upperItem as RulesRelation);
+        } else {
+          (upperItem as RulesRelation).relation = ((upperItem as RulesRelation).rules[0] as RulesRelation).relation;
+          (upperItem as RulesRelation).rules = ((upperItem as RulesRelation).rules[0] as RulesRelation).rules;
+        }
+      }
       return { ...state };
     case 'EDIT_A_RULE':
       Object.keys(action.payload).forEach((key: string) => {
